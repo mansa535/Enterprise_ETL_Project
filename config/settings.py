dfd1844d1d4f8882596af32233bb5398a43d7367
Manasa <manasadonkena40@gmaii.com>
2026-08-28 11:26:44 +0530

    Update project configuration and dependencies

--- config/settings.py
+++ config/settings.py
@@ -3,12 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
-
+STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 SALESFORCE_USERNAME = os.getenv("SALESFORCE_USERNAME")
 SALESFORCE_PASSWORD = os.getenv("SALESFORCE_PASSWORD")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
